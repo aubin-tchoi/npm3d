18601101ead4cae0361cf5c73c6d1fd837abdac8
aubin-tchoi <aubin.tchoi@gmail.com>
2023-02-23 20:53:56 +0100

    perf_monitoring moved out from utils

--- mini_challenge/src/__init__.py
+++ mini_challenge/src/__init__.py
@@ -1,10 +1,9 @@
 from .classification import FeaturesExtractor
+from .perf_monitoring import timeit, checkpoint
 from .ply import read_ply
 from .subsampling import grid_subsampling
 from .utils import (
     print_ram_usage,
     track_time_memory_usage,
     print_tensors_size_in_memory,
-    timeit,
-    checkpoint,
 )
