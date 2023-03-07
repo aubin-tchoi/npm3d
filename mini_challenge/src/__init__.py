febfc953f8052515e034e06c00b9c7822595cebf
aubin-tchoi <aubin.tchoi@gmail.com>
2023-03-07 12:15:18 +0100

    added smoothing strategy using region growing

--- mini_challenge/src/__init__.py
+++ mini_challenge/src/__init__.py
@@ -1,6 +1,7 @@
 from .classification import FeaturesExtractor
 from .perf_monitoring import timeit, checkpoint
 from .ply import read_ply, write_ply
+from .region_growing import smooth_labels
 from .subsampling import grid_subsampling
 from .utils import (
     print_ram_usage,
--- mini_challenge/src/region_growing.py
+++ mini_challenge/src/region_growing.py
@@ -1,7 +1,10 @@
 from queue import Queue
-from typing import Union
+from typing import Union, List, Optional
+
 import numpy as np
 from sklearn.neighbors import KDTree
+
+from .perf_monitoring import timeit
 
 
 def region_criterion(
@@ -17,10 +20,10 @@ def region_criterion(
 
 
 def select_seed(point_cloud: np.ndarray) -> Union[np.ndarray, int]:
-    pass
+    return 0
 
 
-def region_growing(point_cloud, normals, radius: float):
+def region_growing(point_cloud: np.ndarray, radius: float):
     n_points = len(point_cloud)
     region = np.zeros(n_points, dtype=bool)
     visited = np.zeros(n_points, dtype=bool)
@@ -34,34 +37,53 @@ def region_growing(point_cloud, normals, radius: float):
 
     while not queue.empty():
         q = queue.get()
-        point, normal = point_cloud[[q]], normals[[q]]
+        point = point_cloud[[q]]
         neighbors = kdtree.query_radius(point, radius)[0]
 
         neighbors = neighbors[np.logical_not(visited[neighbors])]
         visited[neighbors] = True
+        region[neighbors] = True
 
-        in_region = neighbors[
-            region_criterion(point_cloud[neighbors], point, normals[neighbors], normal)
-        ]
-        region[in_region] = True
-
-        for selected_neighbor in in_region:
+        for selected_neighbor in neighbors:
             queue.put(selected_neighbor)
 
     return region
 
 
-def multi_region_growing(cloud, normals, radius, n_seeds: int):
-    labels = -np.ones(len(cloud), dtype=np.int32)
+def aggregate_labels(
+    labels: np.ndarray, n_labels: int, weights: Optional[List[float]] = None
+) -> int:
+    if weights is None:
+        weights = [1.0 for _ in range(n_labels)]
 
-    for seed in range(n_seeds):
-        remaining_indices = np.flatnonzero(labels == -1)
+    best_label, best_score = -1, 0
+    for label in range(n_labels):
+        if (label_score := weights[label] * (labels == label).sum()) > best_score:
+            best_label = label
+            best_score = label_score
 
+    return best_label
+
+
+@timeit
+def smooth_labels(
+    cloud: np.ndarray,
+    labels: np.ndarray,
+    radius: float,
+    n_labels: int,
+) -> None:
+    non_smooth_area = np.ones(len(cloud), dtype=bool)
+    non_smooth_area[labels == 1] = False
+
+    while non_smooth_area.any():
+        print(f"{non_smooth_area.sum()} points unvisited.")
+        remaining_indices = np.flatnonzero(non_smooth_area)
         region = region_growing(
             cloud[remaining_indices],
-            normals[remaining_indices],
             radius,
         )
-        labels[remaining_indices[region]] = seed
-
-    return labels
+        labels[remaining_indices[region]] = aggregate_labels(
+            labels[remaining_indices[region]], n_labels
+        )
+        non_smooth_area[remaining_indices[region]] = False
+        print(f"{region.sum()} points visited.\n")
